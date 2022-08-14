a5b85008da35a76d5eb1d3066c2d04764044be3c
ACE <elshaer118@gmail.com>
2022-08-14 16:57:58 +0200

    changes

---
+++ Drivers/IntCtrl/IntCtrl.c
@@ -0,0 +1,15 @@
+#include "IntCtrl.h"
+#include
+
+void IntCtrl_init(void)
+{
+  //initiate driver
+  //take configuration and put them into registers
+  //configurations(inside config):
+    //peripheral gates(which interrupt or exception)
+    //group priority
+    //sub-group priority
+  
+   
+  
+}
